9d2a0f727757abe295e3c962616976d671f46354
Víctor Martinez <ahal_tocob@hotmail.com>
2025-04-11 14:42:32 -0600

    Tabla v1

--- src/app/components/lista/lista.component.spec.ts
+++ src/app/components/lista/lista.component.spec.ts
@@ -1,5 +1,4 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ListaComponent } from './lista.component';
 
 describe('ListaComponent', () => {
--- src/app/components/lista/lista.component.ts
+++ src/app/components/lista/lista.component.ts
@@ -1,23 +1,59 @@
 import { Component } from '@angular/core';
 import { NotaService } from '../../services/nota.service';
 import { NotaDto } from '../../interfaces/nota-dto';
+import {MatTableModule} from '@angular/material/table';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {TooltipPosition, MatTooltipModule} from '@angular/material/tooltip';
+import Swal from 'sweetalert2'
 
 @Component({
   selector: 'app-lista',
-  imports: [],
   templateUrl: './lista.component.html',
-  styleUrl: './lista.component.css'
+  styleUrl: './lista.component.css',
+  imports: [MatTableModule, MatButtonModule, MatIconModule, MatProgressSpinnerModule, MatTooltipModule],
 })
 export class ListaComponent {
+  displayColumns: string[] =['nombre','tags','contenido','estado','acciones']
   notas: NotaDto[] = []
+  estaCargando = false
+
   constructor(private servicio: NotaService) {
     this.obtenerNotas()
   }
   obtenerNotas() {
+    this.estaCargando = true
     this.servicio.obtenerTodos().subscribe({
       next: (notas) => { 
         this.notas = notas
+        this.estaCargando = false
+      },error:(error)=>{
+        console.log(error)
+        Swal.fire({
+          icon: "error",
+          title: "Oops...",
+          text: "Valio pepino",   
+          timer: 3000       
+        });
       }
     })
   }
+
+  copiarEnPortapapeles(texto:string){
+    navigator.clipboard.writeText(texto);
+    Swal.fire({
+      position: "top-end",
+      icon: "success",
+      title: "Copiado al portapapeles",
+      showConfirmButton: false,
+      timer: 1000,
+      toast: true
+    });
+  }
 }
+
+// export class Table{
+//   displayColumns: string[] =['nombre', 'contenido','tags','estado','acciones']
+//   dataSource = 
+// }
